0f21e407217f1a5808012e29ba943346e2445c98
Motlalepula Mojatau <pulemojatau@gmail.com>
2025-06-02 19:04:53 +0200

    added second beneficiary form

--- src/components/CareGiverBeneficiary/CareGiverBeneficiary.jsx
+++ src/components/CareGiverBeneficiary/CareGiverBeneficiary.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -32,6 +32,8 @@ const FormContainer = styled.div`
   margin-top: 0px;
   position: relative;
   overflow: hidden;
+  max-width: 420px;
+  width: 100%;
   max-height: calc(100vh - 200px);
 `;
 
@@ -144,71 +146,111 @@ const NextButton = styled.button`
 `;
 
 const CareGiverBeneficiary = () => {
+  const [step, setStep] = useState(1);
+  const [relation, setRelation] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   return (
     <Container>
       <Content>
         <FormContainer>
-          <FormTitle>Follow the steps to add a beneficiary</FormTitle>
-          <FormSubtitle>Please make sure the information is correct</FormSubtitle>
-          
-          <FormGroup>
-            <Label>First Name</Label>
-            <Input type="text" />
-          </FormGroup>
-          
-          <FormGroup>
-            <Label>Last Name</Label>
-            <Input type="text" />
-          </FormGroup>
-          
-          <FormGroup>
-            <Label>Surname</Label>
-            <Input type="text" />
-          </FormGroup>
-          
-          <FormGroup>
-            <Label>Email</Label>
-            <Input type="email" />
-          </FormGroup>
-          
-          <FormGroup>
-            <Label>ID No</Label>
-            <Input type="text" />
-          </FormGroup>
-          
-          <BottomRow>
-            <StepIndicator>
-              <Step active />
-              <Step />
-            </StepIndicator>
-            <NextButton>Next</NextButton>
-          </BottomRow>
+          {step === 1 && (
+            <>
+              <FormTitle>Follow the steps to add a beneficiary</FormTitle>
+              <FormSubtitle>Please make sure the information is correct</FormSubtitle>
+              <FormGroup>
+                <Label>First Name</Label>
+                <Input type="text" />
+              </FormGroup>
+              <FormGroup>
+                <Label>Last Name</Label>
+                <Input type="text" />
+              </FormGroup>
+              <FormGroup>
+                <Label>Surname</Label>
+                <Input type="text" />
+              </FormGroup>
+              <FormGroup>
+                <Label>Email</Label>
+                <Input type="email" />
+              </FormGroup>
+              <FormGroup>
+                <Label>ID No</Label>
+                <Input type="text" />
+              </FormGroup>
+              <BottomRow>
+                <StepIndicator>
+                  <Step active={step === 1} />
+                  <Step active={step === 2} />
+                </StepIndicator>
+                <NextButton onClick={() => setStep(2)}>Next</NextButton>
+              </BottomRow>
+            </>
+          )}
+          {step === 2 && (
+            <>
+              <FormTitle>Complete the last steps to add beneficiary</FormTitle>
+              <FormSubtitle>Please make sure the information is correct</FormSubtitle>
+              <div style={{ color: '#185c37', fontWeight: 600, marginBottom: 10, marginTop: 10 }}>How are you related ?</div>
+              <FormGroup>
+                <Label>Relation: <span style={{ color: '#888', fontSize: 11 }}>(important)</span></Label>
+                <select value={relation} onChange={e => setRelation(e.target.value)} style={{ flex: 1, padding: '6px', borderRadius: 4, border: '1px solid #ddd', fontSize: 12 }}>
+                  <option value="">Choose your relation</option>
+                  <option value="Mother">Mother</option>
+                  <option value="Father">Father</option>
+                  <option value="Sister">Sister</option>
+                  <option value="Brother">Brother</option>
+                  <option value="Other">Other</option>
+                </select>
+              </FormGroup>
+              <div style={{ color: '#185c37', fontWeight: 600, marginBottom: 10, marginTop: 10 }}>Confirm passwords:</div>
+              <FormGroup>
+                <Label>Password: <span style={{ color: '#888', fontSize: 11 }}>(important)</span></Label>
+                <Input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+              </FormGroup>
+              <FormGroup>
+                <Label>Confirm Password</Label>
+                <Input type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
+              </FormGroup>
+              <BottomRow>
+                <StepIndicator>
+                  <Step active={step === 1} />
+                  <Step active={step === 2} />
+                </StepIndicator>
+                <NextButton style={{ background: '#fff', color: '#185c37', border: 'none' }}>
+                  Complete <span style={{ marginLeft: 8 }}>&rarr;</span>
+                </NextButton>
+              </BottomRow>
+            </>
+          )}
         </FormContainer>
-        
-        <Table>
-          <thead>
-            <tr>
-              <Th>Names</Th>
-              <Th>ID Number</Th>
-              <Th>Relation</Th>
-              <Th>Action</Th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <Td>Pulane Thando Malumane</Td>
-              <Td>0001234567</Td>
-              <Td>Daughter</Td>
-              <Td>•••</Td>
-            </tr>
-            <tr>
-              <Td>Pulane Thando Malumane</Td>
-              <Td>0001234567</Td>
-              <Td>Sister</Td>
-              <Td>•••</Td>
-            </tr>
-          </tbody>
-        </Table>
+        {step === 1 && (
+          <Table>
+            <thead>
+              <tr>
+                <Th>Names</Th>
+                <Th>ID Number</Th>
+                <Th>Relation</Th>
+                <Th>Action</Th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <Td>Pulane Thando Malumane</Td>
+                <Td>0001234567</Td>
+                <Td>Daughter</Td>
+                <Td>•••</Td>
+              </tr>
+              <tr>
+                <Td>Pulane Thando Malumane</Td>
+                <Td>0001234567</Td>
+                <Td>Sister</Td>
+                <Td>•••</Td>
+              </tr>
+            </tbody>
+          </Table>
+        )}
       </Content>
     </Container>
   );
