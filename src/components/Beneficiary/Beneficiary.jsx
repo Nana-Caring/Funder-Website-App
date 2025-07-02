4ae86cd9090e92ad8c61282fd97f5136d6e9aa74
Lesedi-coder343 <kameellesedi@gmail.com>
2025-07-02 14:17:12 +0200

    fix: remove duplicate import statement in Beneficiary component

--- src/components/Beneficiary/Beneficiary.jsx
+++ src/components/Beneficiary/Beneficiary.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import editIcon from '../../assets/icons/edit.png';
